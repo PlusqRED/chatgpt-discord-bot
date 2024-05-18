58e466d02d9cc84e8c6d47b64328e9c3593891df
PlusqRED <rickes.oleg@gmail.com>
2024-05-18 23:19:41 +0300

    Refactor EventListener class and MessageCreateListener class
    This commit refactors the EventListener and MessageCreateListener classes to improve readability and performance. Changes include switching from Flux<Void> to Mono<Void> in the processEvent method, moving instance variable declaration to constructor to ensure immutability, and refactoring message processing logic for better error handling and response retrieval.

--- src/main/java/com/aleh/gpt/discord/app/listener/MessageCreateListener.java
+++ src/main/java/com/aleh/gpt/discord/app/listener/MessageCreateListener.java
@@ -4,6 +4,7 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.User;
 import discord4j.core.spec.MessageEditSpec;
+import discord4j.discordjson.possible.Possible;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,21 +13,23 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class MessageCreateListener implements EventListener<MessageCreateEvent> {
 
-    @Value("${discord.bot.answering-speed}")
-    private String discordBotAnsweringSpeed;
-
+    private final String discordBotAnsweringSpeed;
     private final OpenAiChatClient chatClient;
-
     private final AtomicInteger servedUsersCount = new AtomicInteger(0);
 
     @Autowired
-    public MessageCreateListener(OpenAiChatClient chatClient) {
+    public MessageCreateListener(
+            OpenAiChatClient chatClient,
+            @Value("${discord.bot.answering-speed}") String discordBotAnsweringSpeed
+    ) {
         this.chatClient = chatClient;
+        this.discordBotAnsweringSpeed = discordBotAnsweringSpeed;
     }
 
     @Override
@@ -35,35 +38,47 @@ public class MessageCreateListener implements EventListener<MessageCreateEvent> 
     }
 
     @Override
-    public Flux<Void> processEvent(MessageCreateEvent event) {
-        var userMessage = getUserMessage(event);
+    public Mono<Void> processEvent(MessageCreateEvent event) {
+        return getMessageStartingWithEquals(event)
+                .flatMapMany(this::processMessage)
+                .then();
+    }
+
+    private Flux<Message> processMessage(Message message) {
         StringBuilder combinedStringBuilder = new StringBuilder();
-        return sendThinkingResponseMessage(userMessage).flux()
-                .flatMap(message -> updateMessageContent(message, streamAndFilterChatMessages(message), combinedStringBuilder))
-                .doOnComplete(() -> logResultContent(combinedStringBuilder))
-                .then()
-                .flux();
+        return sendEditableThinkingMessage(message)
+                .flatMapMany(thinkingMessage ->
+                        updateMessageContent(thinkingMessage, streamAndFilterChatMessages(message), combinedStringBuilder)
+                )
+                .doOnComplete(() -> logRetrievedCombinedResponse(combinedStringBuilder));
     }
 
-    private void logResultContent(StringBuilder combinedStringBuilder) {
-        if (!combinedStringBuilder.isEmpty()) {
-            LOG.info("[{}] Combined response retrieved: Content: {}", servedUsersCount.incrementAndGet(), combinedStringBuilder);
-        }
+    private Mono<Message> sendEditableThinkingMessage(Message message) {
+        return message.getChannel()
+                .flatMap(channel -> channel.createMessage("Думаю, что бы такого тебе ответить..."));
     }
 
-    private Mono<Message> getUserMessage(MessageCreateEvent event) {
+    private Mono<Message> getMessageStartingWithEquals(MessageCreateEvent event) {
         return Mono.just(event.getMessage())
                 .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(true))
                 .filter(message -> message.getContent().startsWith("=="))
-                .doOnNext(message -> LOG.info("Request retrieved. Author: {}, Content: {}",
-                        message.getAuthor().map(User::getUsername).orElse("Hidden Username"),
-                        message.getContent()
-                ));
+                .doOnNext(MessageCreateListener::logRetrievedRequest);
     }
 
-    private Mono<Message> sendThinkingResponseMessage(Mono<Message> getUserMessage) {
-        return getUserMessage
-                .flatMap(message -> message.getChannel().flatMap(channel -> channel.createMessage("Думаю, что бы такого тебе ответить...")));
+    private static void logRetrievedRequest(Message message) {
+        LOG.info("Request retrieved. Author: {}, Content: {}",
+                message.getAuthor().map(User::getUsername).orElse("Hidden Username"),
+                message.getContent()
+        );
+    }
+
+    private void logRetrievedCombinedResponse(StringBuilder combinedStringBuilder) {
+        if (!combinedStringBuilder.isEmpty()) {
+            LOG.info("[{}] Combined response retrieved: Content: {}",
+                    servedUsersCount.incrementAndGet(),
+                    combinedStringBuilder
+            );
+        }
     }
 
     private Flux<List<String>> streamAndFilterChatMessages(Message message) {
@@ -72,14 +87,24 @@ public class MessageCreateListener implements EventListener<MessageCreateEvent> 
                 .buffer(Integer.parseInt(discordBotAnsweringSpeed));
     }
 
-    private Flux<Message> updateMessageContent(Message message, Flux<List<String>> sendUserMessageToChatClientStream, StringBuilder combinedStringBuilder) {
+    private Flux<Message> updateMessageContent(
+            Message message,
+            Flux<List<String>> sendUserMessageToChatClientStream,
+            StringBuilder combinedStringBuilder
+    ) {
         return sendUserMessageToChatClientStream.flatMap(partOfTheMessage ->
-                message.edit(MessageEditSpec.builder().content(combineStrings(partOfTheMessage, combinedStringBuilder)).build())
+                message.edit(
+                        MessageEditSpec.create()
+                                .withContent(Possible.of(
+                                        Optional.of(combineStrings(partOfTheMessage, combinedStringBuilder)))
+                                )
+                )
         );
     }
 
     private String combineStrings(List<String> partOfTheMessage, StringBuilder combinedStringBuilder) {
-        return partOfTheMessage.stream().collect(() -> combinedStringBuilder, StringBuilder::append, StringBuilder::append).toString();
+        partOfTheMessage.forEach(combinedStringBuilder::append);
+        return combinedStringBuilder.toString();
     }
 
 }
